43f9f80a83b1fe8fac36a55720e0fa33153204a4
iammijan <60599581+iammijan@users.noreply.github.com>
2021-03-18 00:57:59 +0600

    Add files via upload

---
+++ Lab Task 4/PUBLIC HOME.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <title>NDC Company</title>
+  <h1 ><img src="GlobalC4.jpg" alt="company" width="200" height="200"></h1>
+</head>
+<body> 
+<table align="center">
+
+<?php
+echo "Welcome to NDC Company";
+?>
+<tr>
+<td><a href="REGISTRATION.php"> Registration </a></td>
+<td><a href="LOGIN.php"> Login </a></td>
+<td><a href="PUBLIC HOME.php"> Home </a></td>
+</tr>
+</table>
+<?php include 'footer.php';?>
+ </body>
+</html>
---
+++ Lab Task 4/VIEW PROFILE.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <title>Profile</title>
+  <h1 ><img src="GlobalC4.jpg" alt="company" width="200" height="200"></h1>
+</head>
+<body>
+<table align="center">
+	<tr>
+    <td> Logged in <a href="VIEW PROFILE.php"> Smith </a></td>
+    <td><a href="LOGOUT.php"> Logout </a></td>
+    </tr>
+</table>
+
+	<h2>PROFILE</h2>
+	<img src="logo.jpg" alt="logo" width="100" height="100"><br><br>
+	<a href="PROFILE PICTURE"> change </a>
+	<p>Name: Smith</p>
+	<p>Email: Smith@aiub.edu</p>
+	<p>Gender: Male</p>
+	<p>Date of Birth: 05/07/1998</p><br>
+	<a href="EDIT PROFILE.php"> Edit Profile </a>
+
+
+<table align="center">
+<tr>
+<td><a href="LOGGED IN DASHBOARD.php"> Dashboard </a></td><br><br>
+<td><a href="VIEW PROFILE.php"> View Profile </a></td><br><br>
+<td><a href="EDIT PROFILE.php"> Edit Profile </a></td><br><br>
+<td><a href="CHANGE PROFILE PICTURE.php"> Change Profile Picture </a></td><br><br>
+<td><a href="CHANGE PASSWORD.php"> Change Password </a></td><br><br>
+<td><a href="LOGOUT.php"> Logout </a></td><br><br>
+</tr>
+</table>
+<?php include 'footer.php';?>
+ </body>
+</html>
